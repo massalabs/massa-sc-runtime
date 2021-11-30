8f2c3fe3a5e42342d27754ca95cf4f5b1902afea
Yvan Sraka <yvan@sraka.xyz>
2021-11-30 16:03:09 +0100

    Use serde to pass args to `hello.js` as JSON

--- examples/hello.js
+++ examples/hello.js
@@ -1,1 +1,6 @@
-console.log(`Hello, World!`)
+// This could be hide into `massa` module
+import { getenv } from "std"
+const args = JSON.parse(getenv("_args"));
+
+console.log(`Hello, ${args.name}`)
+// Now you,re ready to write a JS smart contract
--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,20 @@
 //!   2. Attach the imports from the `WasiEnv` to a new instance
 //!   3. Run the `WASI` module.
 
+use serde::Serialize;
 use wasmer::{Instance, Module, Store};
 use wasmer_compiler_cranelift::Cranelift;
 use wasmer_engine_universal::Universal;
 use wasmer_wasi::WasiState;
 
+#[derive(Serialize)]
+struct Data {
+    name: String,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Stage 0: load up in memory wasm module
+
     let wasm_path = concat!(env!("CARGO_MANIFEST_DIR"), "/static/qjs.wasm");
 
     // Let's declare the Wasm module with the text representation.
@@ -28,14 +36,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Let's compile the Wasm module.
     let module = Module::new(&store, wasm_bytes)?;
 
+    // Stage 1: Executing wasm module
+    // (for running another JS file we restart here without relauching stage 0)
+
+    // TODO: create a pipe to handle stdout
+    // let pipe = wasmer_wasi::Pipe::new();
+
+    // Pass complex types encoded as JSON as input
+    let args = serde_json::to_string(&Data {
+        name: "world".to_string(),
+    })?;
+
     println!("Creating `WasiEnv`...");
     // First, we create the `WasiEnv`
     let mut wasi_env = WasiState::new("hello")
-        // TODO: .stdin()
-        // https://docs.rs/wasmer-wasi/2.0.0/wasmer_wasi/struct.WasiStateBuilder.html#method.stdin
-        // https://docs.rs/wasmer-wasi/2.0.0/wasmer_wasi/struct.WasiFs.html#method.stdin
+        .env("_args", args)
         .args(&["examples/hello.js"])
         .preopen_dir("examples")?
+        // .stdout(Box::new(file))
         .finalize()?;
 
     println!("Instantiating module with WASI imports...");
@@ -48,6 +66,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // And we just call the `_start` function!
     let start = instance.exports.get_function("_start")?;
     start.call(&[])?;
+
+    // let mut s = String::new();
+    // let output = pipe.read_to_string(&mut s)?;
+    // assert_eq!(output, "Hello, World!");
 
     Ok(())
 }
