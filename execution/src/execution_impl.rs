b09f1e13faad7254c5232fe7de525fe1d7e310d8
Adrien Zinger <zinger.ad@gmail.com>
2021-12-07 09:53:02 +0100

    Fix format

--- execution/src/execution_impl.rs
+++ execution/src/execution_impl.rs
@@ -1,11 +1,14 @@
-use std::sync::Arc;
-use wasmer::{Function, ImportObject, Instance, Module, Store, Val, imports, Cranelift, Universal, CompilerConfig};
 use crate::api;
+use crate::env::{abort, get_remaining_points, Env};
 use crate::types::{Address, Response};
 use anyhow::{bail, Result};
-use wasmer_as::{StringPtr, Read as ASRead, Write as ASWrite, };
-use crate::env::{Env, abort, get_remaining_points};
+use std::sync::Arc;
 use wasmer::wasmparser::Operator;
+use wasmer::{
+    imports, CompilerConfig, Cranelift, Function, ImportObject, Instance, Module, Store, Universal,
+    Val,
+};
+use wasmer_as::{Read as ASRead, StringPtr, Write as ASWrite};
 use wasmer_middlewares::Metering;
 
 const MAIN: &str = "main";
@@ -13,7 +16,12 @@ const MAIN: &str = "main";
 fn print(env: &Env, arg: i32) {
     let str_ptr = StringPtr::new(arg as u32);
     // May be in wasmer-as create a pointer that keep memory?
-    println!("\n{}", str_ptr.read(env.wasm_env.memory.get_ref().expect("initialized memory")).unwrap());
+    println!(
+        "\n{}",
+        str_ptr
+            .read(env.wasm_env.memory.get_ref().expect("initialized memory"))
+            .unwrap()
+    );
 }
 
 fn call(env: &Env, address: i32, function: i32) -> i32 {
@@ -24,7 +32,8 @@ fn call(env: &Env, address: i32, function: i32) -> i32 {
     let address = &addr_ptr.read(memory).unwrap();
     let fnc = &func_ptr.read(memory).unwrap();
     let module = &api::get_module(address).unwrap();
-    let value = super::execution_impl::exec(env.remaining_points, None, module, fnc, &vec![]).unwrap();
+    let value =
+        super::execution_impl::exec(env.remaining_points, None, module, fnc, &vec![]).unwrap();
     let ret = StringPtr::alloc(&value.ret, &env.wasm_env).unwrap();
     ret.offset() as i32
 }
@@ -49,10 +58,16 @@ fn create_instance(limit: u64, module: &[u8]) -> Result<Instance> {
 
 /// fnc: function name
 /// params: function arguments
-pub fn exec(limit: u64, instance: Option<Instance>, module: &[u8], fnc: &str, params: &[i32]) -> Result<Response> {
+pub fn exec(
+    limit: u64,
+    instance: Option<Instance>,
+    module: &[u8],
+    fnc: &str,
+    params: &[i32],
+) -> Result<Response> {
     let instance = match instance {
         Some(instance) => instance,
-        None => create_instance(limit, module)?
+        None => create_instance(limit, module)?,
     };
     let mut p = vec![];
     for param in params {
@@ -63,28 +78,35 @@ pub fn exec(limit: u64, instance: Option<Instance>, module: &[u8], fnc: &str, pa
         Ok(value) => {
             // todo: clean and define wat should be return by the main
             if fnc.eq(MAIN) {
-                return Ok(Response{ ret: "0".to_string(), remaining_points: 0});
+                return Ok(Response {
+                    ret: "0".to_string(),
+                    remaining_points: 0,
+                });
             }
             let str_ptr = StringPtr::new(value.get(0).unwrap().i32().unwrap() as u32);
             let memory = instance.exports.get_memory("memory")?;
-            Ok(Response { ret: str_ptr.read(memory)?, remaining_points: get_remaining_points(&instance) })
-        },
-        Err(error) => bail!(error)
+            Ok(Response {
+                ret: str_ptr.read(memory)?,
+                remaining_points: get_remaining_points(&instance),
+            })
+        }
+        Err(error) => bail!(error),
     }
 }
 
 pub fn run(address: Address, module: &[u8], limit: u64) -> Result<()> {
     let instance = create_instance(limit, module)?;
     // todo: what to export?
-    println!("Module inserted at {} by {}",
-        api::insert_module(address.clone(), module), address);
+    println!(
+        "Module inserted at {} by {}",
+        api::insert_module(address.clone(), module),
+        address
+    );
     if instance.exports.contains(MAIN) {
         return match exec(limit, Some(instance), module, MAIN, &[]) {
-            Ok(_) => {
-                Ok(())
-            },
-            Err(error) => bail!(error)
-        }
+            Ok(_) => Ok(()),
+            Err(error) => bail!(error),
+        };
     }
     Ok(())
 }
