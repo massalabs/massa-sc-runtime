6d8cbeaad27e8888be9b973fa18fb3c434ffc679
Adrien Zinger <zinger.ad@gmail.com>
2021-12-07 09:45:41 +0100

    Implement metering
    - extends the env of wasmer-as
    - add metering with wasmer
    - fix the main temporary

--- execution/src/execution_impl.rs
+++ execution/src/execution_impl.rs
@@ -1,33 +1,39 @@
-use wasmer::{Function, ImportObject, Instance, Module, Store, Val, imports};
+use std::sync::Arc;
+use wasmer::{Function, ImportObject, Instance, Module, Store, Val, imports, Cranelift, Universal, CompilerConfig};
 use crate::api;
-use crate::types::Address;
+use crate::types::{Address, Response};
 use anyhow::{bail, Result};
-use wasmer_as::{Env, abort, StringPtr, Read as ASRead, Write as ASWrite};
+use wasmer_as::{StringPtr, Read as ASRead, Write as ASWrite, };
+use crate::env::{Env, abort, get_remaining_points};
+use wasmer::wasmparser::Operator;
+use wasmer_middlewares::Metering;
 
 const MAIN: &str = "main";
 
 fn print(env: &Env, arg: i32) {
     let str_ptr = StringPtr::new(arg as u32);
     // May be in wasmer-as create a pointer that keep memory?
-    println!("\n{}", str_ptr.read(env.memory.get_ref().expect("initialized memory")).unwrap());
+    println!("\n{}", str_ptr.read(env.wasm_env.memory.get_ref().expect("initialized memory")).unwrap());
 }
 
 fn call(env: &Env, address: i32, function: i32) -> i32 {
-    let memory = env.memory.get_ref().expect("initialized memory");
+    let memory = env.wasm_env.memory.get_ref().expect("initialized memory");
     let addr_ptr = StringPtr::new(address as u32);
     let func_ptr = StringPtr::new(function as u32);
 
     let address = &addr_ptr.read(memory).unwrap();
     let fnc = &func_ptr.read(memory).unwrap();
     let module = &api::get_module(address).unwrap();
-
-    let value = super::execution_impl::exec(None, module, fnc, &vec![]).unwrap();
-    let ret = StringPtr::alloc(&value, env).unwrap();
+    let value = super::execution_impl::exec(env.remaining_points, None, module, fnc, &vec![]).unwrap();
+    let ret = StringPtr::alloc(&value.ret, &env.wasm_env).unwrap();
     ret.offset() as i32
 }
 
-fn create_instance(module: &[u8]) -> Result<Instance> {
-    let store = Store::default();
+fn create_instance(limit: u64, module: &[u8]) -> Result<Instance> {
+    let metering = Arc::new(Metering::new(limit, |_: &Operator| -> u64 { 1 }));
+    let mut compiler_config = Cranelift::default();
+    compiler_config.push_middleware(metering);
+    let store = Store::new(&Universal::new(compiler_config).engine());
     let resolver: ImportObject = imports! {
         "env" => {
             "abort" =>  Function::new_native_with_env(&store, Env::default(), abort)
@@ -43,10 +49,10 @@ fn create_instance(module: &[u8]) -> Result<Instance> {
 
 /// fnc: function name
 /// params: function arguments
-pub fn exec(instance: Option<Instance>, module: &[u8], fnc: &str, params: &[i32]) -> Result<String> {
+pub fn exec(limit: u64, instance: Option<Instance>, module: &[u8], fnc: &str, params: &[i32]) -> Result<Response> {
     let instance = match instance {
         Some(instance) => instance,
-        None => create_instance(module)?
+        None => create_instance(limit, module)?
     };
     let mut p = vec![];
     for param in params {
@@ -57,23 +63,23 @@ pub fn exec(instance: Option<Instance>, module: &[u8], fnc: &str, params: &[i32]
         Ok(value) => {
             // todo: clean and define wat should be return by the main
             if fnc.eq(MAIN) {
-                return Ok("0".to_string());
+                return Ok(Response{ ret: "0".to_string(), remaining_points: 0});
             }
             let str_ptr = StringPtr::new(value.get(0).unwrap().i32().unwrap() as u32);
             let memory = instance.exports.get_memory("memory")?;
-            Ok(str_ptr.read(memory)?)
+            Ok(Response { ret: str_ptr.read(memory)?, remaining_points: get_remaining_points(&instance) })
         },
         Err(error) => bail!(error)
     }
 }
 
-pub fn run(address: Address, module: &[u8]) -> Result<()> {
-    let instance = create_instance(module)?;
+pub fn run(address: Address, module: &[u8], limit: u64) -> Result<()> {
+    let instance = create_instance(limit, module)?;
     // todo: what to export?
     println!("Module inserted at {} by {}",
         api::insert_module(address.clone(), module), address);
     if instance.exports.contains(MAIN) {
-        return match exec(Some(instance), module, MAIN, &[]) {
+        return match exec(limit, Some(instance), module, MAIN, &[]) {
             Ok(_) => {
                 Ok(())
             },
--- execution/src/types.rs
+++ execution/src/types.rs
@@ -1,3 +1,11 @@
 pub type Address = String;
 pub type Bytecode = Vec::<u8>;
 pub type Ledger = std::collections::BTreeMap<Address, Bytecode>; // Byttecode instead of String
+
+/// That's what is returned when a module is executed correctly since the end
+pub struct Response {
+    /// returned value from the module call
+    pub ret: String,
+    /// number of points that remain after the execution (metering)
+    pub remaining_points: u64,
+}
