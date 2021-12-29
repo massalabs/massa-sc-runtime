5f289ae263b1d1527bcdc8dcd7473b3351e4cd38
Adrien Zinger <zinger.ad@gmail.com>
2021-12-29 10:50:58 +0100

    Add match include_base64 unit tests

--- wasm/massa_tools/include_base64.js
+++ wasm/massa_tools/include_base64.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const file = process.argv[2];
-const include_bytes_regex = /include_base64\(["']+([\.a-z_\-\/\\]*)["']+\)[;]+/i;
+const match = require('./match_include_base64');
 const lines = String(fs.readFileSync(file)).split('\n').map(line => {
-    let res = line.match(include_bytes_regex);
+    let res = match(line);
     if (res != null) {
         const data = fs.readFileSync(res[1], 'base64');
         line = line.replace(res[0], JSON.stringify(data));
