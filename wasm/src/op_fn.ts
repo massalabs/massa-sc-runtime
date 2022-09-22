fb9a1377a38cc99d49fea2a195c140cff3467fd3
sydhds <sydhds@massa.net>
2022-09-22 16:22:39 +0200

    Use massa-as-sdk (was massa-sc-std)

--- wasm/src/op_fn.ts
+++ wasm/src/op_fn.ts
@@ -1,7 +1,7 @@
-import { getOpKeys, hasOpKey, getOpData, print } from "@massalabs/massa-sc-std";
+import { getOpKeys, hasOpKey, getOpData, print } from "@massalabs/massa-as-sdk";
 
 export function main(_args: string): void {
-    let keys: Array<StaticArray<u8>> = getOpKeys();
+    let keys: Array<Uint8Array> = getOpKeys();
     for(let i=0; i<keys.length; i++) {
         let k = keys[i];
         if (hasOpKey(k)) {
