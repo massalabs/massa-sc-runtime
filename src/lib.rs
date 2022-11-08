abe048761cc70af6e5ec2a086fa4442202808266
AurelienFT <aurelien.foucault@epitech.eu>
2022-11-08 10:59:57 +0100

    Add export of gc function with the gc feature

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ mod tunable_memory;
 mod types;
 
 pub use execution_impl::{run_function, run_main};
+#[cfg(feature = "gas_calibration")]
+pub use execution_impl::run_main_gc;
 pub use types::*;
 
 mod middlewares;
