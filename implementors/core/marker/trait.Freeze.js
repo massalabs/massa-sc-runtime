c9fb569b2a832e7d225ca1ae74a9a53d7781197f
AurelienFT <AurelienFT@users.noreply.github.com>
2022-12-22 10:57:33 +0000

    Deploying to gh-pages from @ massalabs/massa-sc-runtime@5a0305afba45e293364396430317d2e738667d58 🚀

---
+++ implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"massa_sc_runtime":[["impl Freeze for <a class=\"struct\" href=\"massa_sc_runtime/struct.GasCosts.html\" title=\"struct massa_sc_runtime::GasCosts\">GasCosts</a>",1,["massa_sc_runtime::types::GasCosts"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
