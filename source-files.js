bd79d084f4a6151a1caac47b3b2b0cd26af21783
Eitu33 <Eitu33@users.noreply.github.com>
2023-02-09 08:53:51 +0000

    Deploying to gh-pages from @ massalabs/massa-sc-runtime@16a0cbe723f6e8cb0f3db7b22f06f01c863325c6 🚀

--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"massa_sc_runtime":["",[["as_execution",[],["abi.rs","abi_error.rs","common.rs","execution.rs","mod.rs"]],["env",[],["as_env.rs","mod.rs"]],["middlewares",[],["gas_calibration.rs","mod.rs","operator.rs"]]],["execution_impl.rs","lib.rs","settings.rs","tunable_memory.rs","types.rs"]]\
+"massa_sc_runtime":["",[["as_execution",[],["abi.rs","common.rs","context.rs","error.rs","mod.rs"]],["env",[],["as_env.rs","mod.rs"]],["middlewares",[],["gas_calibration.rs","mod.rs","operator.rs"]]],["execution_impl.rs","lib.rs","settings.rs","tunable_memory.rs","types.rs"]]\
 }');
 createSourceSidebar();
