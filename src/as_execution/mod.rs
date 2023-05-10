37f610ed6cd29d7889b4112620542daf0d87d423
Modship <yeskinokay@gmail.com>
2023-05-10 08:46:19 +0200

    update wasmer to 3.3.0 (#249)

--- src/as_execution/mod.rs
+++ src/as_execution/mod.rs
@@ -13,8 +13,8 @@ use crate::tunable_memory::LimitingTunables;
 use crate::{GasCosts, Interface, Response};
 use anyhow::Result;
 use std::sync::Arc;
-use wasmer::{wasmparser::Operator, BaseTunables, EngineBuilder, Pages, Target};
-use wasmer::{CompilerConfig, Cranelift, Engine, Features, Module, Store};
+use wasmer::{wasmparser::Operator, BaseTunables, Engine, EngineBuilder, Pages, Target};
+use wasmer::{CompilerConfig, Cranelift, Features, Module, Store};
 use wasmer_compiler_singlepass::Singlepass;
 use wasmer_middlewares::metering::MeteringPoints;
 use wasmer_middlewares::{metering, Metering};
@@ -61,7 +61,7 @@ impl ASModule {
     pub fn deserialize(ser_module: &[u8], limit: u64, gas_costs: GasCosts) -> Result<Self> {
         // Deserialization is only meant for Cranelift modules
         let engine = init_cl_engine(limit, gas_costs);
-        let store = init_store(&engine)?;
+        let store = Store::new(engine.clone());
         // Unsafe because code injection is possible
         // That's not an issue because we only deserialize modules we have serialized by ourselves before
         let module = unsafe { Module::deserialize(&store, ser_module)? };
@@ -128,9 +128,16 @@ pub(crate) fn init_sp_engine(limit: u64, gas_costs: GasCosts) -> Engine {
         compiler_config.push_middleware(metering);
     }
 
-    EngineBuilder::new(compiler_config)
-        .set_features(Some(FEATURES))
-        .engine()
+    let base = BaseTunables::for_target(&Target::default());
+    let tunables = LimitingTunables::new(base, Pages(max_number_of_pages()));
+
+    let mut engine = Engine::from(
+        EngineBuilder::new(compiler_config)
+            .set_features(Some(FEATURES))
+            .engine(),
+    );
+    engine.set_tunables(tunables);
+    engine
 }
 
 pub(crate) fn init_cl_engine(limit: u64, gas_costs: GasCosts) -> Engine {
@@ -159,18 +166,17 @@ pub(crate) fn init_cl_engine(limit: u64, gas_costs: GasCosts) -> Engine {
         compiler_config.push_middleware(metering);
     }
 
-    EngineBuilder::new(compiler_config)
-        .set_features(Some(FEATURES))
-        .engine()
-}
-
-pub(crate) fn init_store(engine: &Engine) -> Result<Store> {
     let base = BaseTunables::for_target(&Target::default());
     let tunables = LimitingTunables::new(base, Pages(max_number_of_pages()));
-    let store = Store::new_with_tunables(engine, tunables);
-    Ok(store)
-}
 
+    let mut engine = Engine::from(
+        EngineBuilder::new(compiler_config)
+            .set_features(Some(FEATURES))
+            .engine(),
+    );
+    engine.set_tunables(tunables);
+    engine
+}
 /// Internal execution function, used on smart contract called from node or
 /// from another smart contract
 /// Parameters:
@@ -196,7 +202,7 @@ pub(crate) fn exec_as_module(
         Compiler::CL => init_cl_engine(limit, gas_costs.clone()),
         Compiler::SP => init_sp_engine(limit, gas_costs.clone()),
     };
-    let mut store = init_store(&engine)?;
+    let mut store = Store::new(engine);
     let mut context = ASContext::new(interface, as_module.binary_module, gas_costs);
     let (instance, init_rem_points) = context.create_vm_instance_and_init_env(&mut store)?;
     let init_cost = as_module.initial_limit.saturating_sub(init_rem_points);
--- src/tests/tests_runtime.rs
+++ src/tests/tests_runtime.rs
@@ -1,4 +1,4 @@
-use crate::as_execution::{init_store, ASContext, ASModule};
+use crate::as_execution::{ASContext, ASModule};
 use crate::tests::{Ledger, TestInterface};
 use crate::Compiler;
 use crate::{
@@ -10,6 +10,7 @@ use parking_lot::Mutex;
 use rand::Rng;
 use serial_test::serial;
 use std::sync::Arc;
+use wasmer::Store;
 use wasmer::WasmPtr;
 
 #[test]
@@ -412,15 +413,12 @@ fn test_class_id() {
         "/wasm/return_basic.wasm"
     ));
     let module = ASModule::new(bytecode, 100_000, GasCosts::default(), Compiler::SP).unwrap();
-    let mut store = init_store(&module._engine).unwrap();
+    let mut store = Store::new(module._engine);
     let mut context = ASContext::new(&*interface, module.binary_module, GasCosts::default());
     let (instance, _) = context.create_vm_instance_and_init_env(&mut store).unwrap();
 
     // setup test specific context
     let (_, fenv) = context.resolver(&mut store);
-    let fenv_mut = fenv.into_mut(&mut store);
-    let memory = context.env.get_ffi_env().memory.as_ref().unwrap();
-    let memory_view = memory.view(&fenv_mut);
 
     // get string and array offsets
     let return_string = instance.exports.get_function("return_string").unwrap();
@@ -439,6 +437,10 @@ fn test_class_id() {
         .unwrap()
         .i32()
         .unwrap();
+
+    let memory = context.env.get_ffi_env().memory.as_ref().unwrap();
+    let fenv_mut = fenv.into_mut(&mut store);
+    let memory_view = memory.view(&fenv_mut);
 
     // use `u32` size to retrieve the class id
     // see https://www.assemblyscript.org/runtime.html#memory-layout
