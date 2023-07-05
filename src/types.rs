8ad3296d211976b912f909dca8372fd5ef074638
Leo-Besancon <lb@massa.net>
2023-07-05 16:48:17 +0200

    Feature/improve abi types in wasmv1 leo (#277)
    * Sync with massa-proto-rs after massa-proto changes
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    
    * Added check / get_version / time abis
    
    Sync
    
    * Target proto-rs leo branch
    
    * Sync
    
    * Test exhaustive sc + return non empty sc address in create_sc test interface
    
    * Update target
    
    * Updated wasm
    
    ---------
    
    Signed-off-by: Jean-François Morcillo <jfm@massa.net>
    Co-authored-by: Jean-François Morcillo <jfm@massa.net>

--- src/types.rs
+++ src/types.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow, bail, Result};
-use massa_proto_rs::massa::model::v1::{NativeAmount, Slot};
+use massa_proto_rs::massa::model::v1::{NativeAmount, Slot, NativeTime, AddressCategory};
 use serde::{de::DeserializeOwned, Serialize};
 use std::{
     collections::{BTreeSet, HashMap},
@@ -643,6 +643,95 @@ pub trait Interface: Send + Sync + InterfaceClone {
         unimplemented!("div_rem_native_amounts_wasmv1");
     }
 
+    fn check_address_wasmv1(
+        &self,
+        to_check: &String,
+    ) -> Result<bool> {
+        unimplemented!("check_address_wasmv1");
+    }
+
+    fn check_pubkey_wasmv1(
+        &self,
+        to_check: &String,
+    ) -> Result<bool> {
+        unimplemented!("check_pubkey_wasmv1");
+    }
+
+    fn check_signature_wasmv1(
+        &self,
+        to_check: &String,
+    ) -> Result<bool> {
+        unimplemented!("check_signature_wasmv1");
+    }
+
+    fn get_address_category_wasmv1(
+        &self,
+        to_check: &String,
+    ) -> Result<AddressCategory> {
+        unimplemented!("get_address_category_wasmv1");
+    }
+
+    fn get_address_version_wasmv1(
+        &self,
+        address: &String,
+    ) -> Result<u64> {
+        unimplemented!("get_address_version_wasmv1");
+    }
+
+    fn get_pubkey_version_wasmv1(
+        &self,
+        pubkey: &String,
+    ) -> Result<u64> {
+        unimplemented!("get_pubkey_version_wasmv1");
+    }
+
+    fn get_signature_version_wasmv1(
+        &self,
+        signature: &String,
+    ) -> Result<u64> {
+        unimplemented!("get_signature_version_wasmv1");
+    }
+
+    fn checked_add_native_time_wasmv1(
+        &self,
+        time1: &NativeTime,
+        time2: &NativeTime,
+    ) -> Result<NativeTime> {
+        unimplemented!("checked_add_native_time_wasmv1");
+    }
+
+    fn checked_sub_native_time_wasmv1(
+        &self,
+        time1: &NativeTime,
+        time2: &NativeTime,
+    ) -> Result<NativeTime> {
+        unimplemented!("checked_sub_native_time_wasmv1");
+    }
+
+    fn checked_mul_native_time_wasmv1(
+        &self,
+        time: &NativeTime,
+        factor: u64,
+    ) -> Result<NativeTime> {
+        unimplemented!("checked_mul_native_time_wasmv1");
+    }
+
+    fn checked_scalar_div_native_time_wasmv1(
+        &self,
+        dividend: &NativeTime,
+        divisor: u64,
+    ) -> Result<(NativeTime, NativeTime)> {
+        unimplemented!("checked_scalar_div_native_time_wasmv1");
+    }
+
+    fn checked_div_native_time_wasmv1(
+        &self,
+        dividend: &NativeTime,
+        divisor: &NativeTime,
+    ) -> Result<(u64, NativeTime)> {
+        unimplemented!("checked_div_native_time_wasmv1");
+    }
+    
     fn base58_check_to_bytes_wasmv1(&self, s: &str) -> Result<Vec<u8>>;
     fn bytes_to_base58_check_wasmv1(&self, bytes: &[u8]) -> String;
 }
