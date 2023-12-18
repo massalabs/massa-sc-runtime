d8f8b5d34060fb3366b6baa8feb88fc60a47affa
Leo-Besancon <Leo-Besancon@users.noreply.github.com>
2023-12-18 08:51:44 +0000

    Deploying to gh-pages from @ massalabs/massa-sc-runtime@29b53a4aa40bedc38e261410c36d81c9a5d78639 🚀

--- implementors/core/error/trait.Error.js
+++ implementors/core/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"massa_sc_runtime":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"massa_sc_runtime/enum.VMError.html\" title=\"enum massa_sc_runtime::VMError\">VMError</a>"]]
+"massa_sc_runtime":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"massa_sc_runtime/enum.VMError.html\" title=\"enum massa_sc_runtime::VMError\">VMError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
