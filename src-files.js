676ed583a6bb05dbfa454954982d8f1ee210009b
modship <modship@users.noreply.github.com>
2024-08-28 12:36:08 +0000

    Deploying to gh-pages from @ massalabs/massa-sc-runtime@960bf4e1873cdbae01a0494de62c6d16cae5bc9a 🚀

--- src-files.js
+++ src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = JSON.parse('{\
-"massa_sc_runtime":["",[["as_execution",[],["abi.rs","common.rs","context.rs","env.rs","error.rs","mod.rs"]],["middlewares",[],["condom.rs","dumper.rs","gas_calibration.rs","mod.rs","operator.rs"]],["wasmv1_execution",[["abi",[],["abis.rs","handler.rs","mod.rs"]]],["env.rs","error.rs","ffi.rs","mod.rs"]]],["error.rs","execution.rs","lib.rs","settings.rs","tunable_memory.rs","types.rs"]]\
+"massa_sc_runtime":["",[["as_execution",[],["abi.rs","common.rs","context.rs","env.rs","error.rs","mod.rs"]],["middlewares",[],["dumper.rs","gas_calibration.rs","mod.rs","operator.rs"]],["wasmv1_execution",[["abi",[],["abis.rs","handler.rs","mod.rs"]]],["env.rs","error.rs","ffi.rs","mod.rs"]]],["error.rs","execution.rs","lib.rs","settings.rs","tunable_memory.rs","types.rs"]]\
 }');
 createSrcSidebar();
