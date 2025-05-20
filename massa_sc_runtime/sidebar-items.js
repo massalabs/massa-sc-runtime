61ce8d416ab456fd0064163b67bc355806eb65e7
Leo-Besancon <Leo-Besancon@users.noreply.github.com>
2025-05-20 15:08:14 +0000

    Deploying to gh-pages from @ massalabs/massa-sc-runtime@de3d57f8be3e4b1b3981d1ab99e46c5ccb588689 🚀

--- massa_sc_runtime/sidebar-items.js
+++ massa_sc_runtime/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Compiler","RuntimeModule","VMError"],"fn":["run_function","run_main"],"struct":["CondomLimits","GasCosts","Response"],"trait":["Interface","InterfaceClone"]};
+window.SIDEBAR_ITEMS = {"enum":["Compiler","InterfaceError","RuntimeModule","VMError"],"fn":["run_function","run_main"],"macro":["bail"],"struct":["CondomLimits","GasCosts","Response"],"trait":["Interface","InterfaceClone"],"type":["Result"]};
